a9dcaf64bd9d0fb118f561b43266a2669542240c
Darkknight123 <philipkerry57@gmail.com>
2022-03-24 11:24:09 +0300

    model

--- app/src/main/java/com/example/forecastapp/api/ApiService.kt
+++ app/src/main/java/com/example/forecastapp/api/ApiService.kt
@@ -1,7 +1,10 @@
 package com.example.forecastapp.api
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiService {
 
     @GET("weather/Nairobi")
-    suspend fun getWeather():
+    suspend fun getWeather(): Response<Weather>
 }
