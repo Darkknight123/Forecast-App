ffdad0c44ca05399e4df4849613e766d76627b67
Darkknight123 <philipkerry57@gmail.com>
2022-03-30 15:52:32 +0300

    finished with the logic side code

--- app/src/main/java/com/example/forecastapp/MainActivity.kt
+++ app/src/main/java/com/example/forecastapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.forecastapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.forecastapp.databinding.ActivityMainBinding
+import com.example.forecastapp.viewmodel.WeatherViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel:WeatherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        viewModel.weatherResp.observe(this) { weather ->
+
+            binding.apply {
+
+                tvTemperature.text = weather.temperature
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/forecastapp/MyApplication.kt
+++ app/src/main/java/com/example/forecastapp/MyApplication.kt
@@ -1,8 +1,8 @@
-package com.example.forecastapp.utils
+package com.example.forecastapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
-class MyApplication: Application()
+class MyApplication:Application()
